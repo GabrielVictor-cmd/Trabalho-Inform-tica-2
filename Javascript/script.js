5fef9cd64e688d155c3d302bab294f4bedf4225a
Gabriel Victor <gabrielvictorribeiro84@gmail.com>
2025-05-30 20:04:23 -0300

    Trabalho de Javascript

---
+++ Javascript/script.js
@@ -0,0 +1,8 @@
+// Um simples alerta na tela
+alert("Olá! Este é um exemplo de JavaScript");
+
+// Soma de dois números
+let a = 5
+let b = 10
+let resultado = a + b
+console.log("O resultado da soma é: " + resultado)
